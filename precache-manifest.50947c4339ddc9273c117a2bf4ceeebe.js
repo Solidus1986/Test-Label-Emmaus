c2cc281c25977f69783c8af37edfe3b9315bf349
Solidus1986 <jonathan.bibinet@gmail.com>
2020-11-10 11:54:26 +0100

    Updates

---
+++ precache-manifest.50947c4339ddc9273c117a2bf4ceeebe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e95cbd3395692f0379c5ac1c451a82",
+    "url": "/find-school/index.html"
+  },
+  {
+    "revision": "057380ce2bba9971e129",
+    "url": "/find-school/static/css/main.1e91749d.chunk.css"
+  },
+  {
+    "revision": "1327dbe7ff32e7ea2767",
+    "url": "/find-school/static/js/2.53bc3875.chunk.js"
+  },
+  {
+    "revision": "628529191ee8c5edc162926910ee37b3",
+    "url": "/find-school/static/js/2.53bc3875.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "057380ce2bba9971e129",
+    "url": "/find-school/static/js/main.451761dd.chunk.js"
+  },
+  {
+    "revision": "aab8cf314cbff2eefbac",
+    "url": "/find-school/static/js/runtime-main.063969e3.js"
+  },
+  {
+    "revision": "79229d848f67f7718ba04990221a76a6",
+    "url": "/find-school/static/media/icon-more.79229d84.svg"
+  },
+  {
+    "revision": "a736865a1213c869968010806a2a2b05",
+    "url": "/find-school/static/media/img1.a736865a.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/find-school/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c4cf322743a6d2e93a4a23a2cb7a7a8e",
+    "url": "/find-school/static/media/school.c4cf3227.svg"
+  }
+]);
